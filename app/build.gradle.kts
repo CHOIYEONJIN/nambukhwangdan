ae15b070c48642bf0ee05d98cfd4792fef60f0bf
최연진 <yeonjin9119@sookmyung.ac.kr>
2025-11-26 12:55:17 +0900

    자잘한 오류 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,8 @@ android {
 }
 
 dependencies {
+    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.material.icons.extended)
     implementation(libs.coil.compose)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
--- app/src/main/java/com/example/nambukhwangdan/navigation/NavGraph.kt
+++ app/src/main/java/com/example/nambukhwangdan/navigation/NavGraph.kt
@@ -7,12 +7,12 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.example.nambukhwangdan.screens.calender.EmotionCalendarScreen
-import com.example.nambukhwangdan.screens.diary.AnalyzeLoadingScreen
+import com.example.nambukhwangdan.screens.diary.AnalyzeLoadingOverlay
 import com.example.nambukhwangdan.screens.diary.AnalyzeResultScreen
 import com.example.nambukhwangdan.screens.diary.DiaryWriteScreen
 import com.example.nambukhwangdan.screens.diary.LetterToTomorrowScreen
 import com.example.nambukhwangdan.screens.home.HomeScreen
-import com.example.nambukhwangdan.screens.journal.journalScreen
+import com.example.nambukhwangdan.screens.journal.JournalScreen
 import com.example.nambukhwangdan.screens.letters.NewLetterScreen
 import com.example.nambukhwangdan.screens.letters.ReplyScreen
 import com.example.nambukhwangdan.screens.onboarding.LoginScreen
@@ -30,7 +30,7 @@ fun NavGraph(navController: NavHostController) {
         // route 가 list일 떄 TodoListScreen으로 이동함
         // 할 일 목록 화면으로 이동한다
         composable("list") {
-            journalScreen(viewModel, navController)
+            JournalScreen(viewModel)
         }
         // route 가 addEdit일 때 AddEditTodoScreen으로 이동함
         // 새로운 todo를 만드는 화면으로 이동할 때 사용하는 route
@@ -38,7 +38,7 @@ fun NavGraph(navController: NavHostController) {
             EmotionCalendarScreen()
         }
         composable(route="AnalyzeLoadingScreen") {
-            AnalyzeLoadingScreen(viewModel,navController)
+            AnalyzeLoadingOverlay(navController)
         }
         composable(route="AnalyzeResultScreen"){
             AnalyzeResultScreen(viewModel,navController)
@@ -72,7 +72,7 @@ fun NavGraph(navController: NavHostController) {
 
             DiaryWriteScreen(
                 viewModel = viewModel,
-                navController = navController
+                bottomNavController = navController
             )
         }
         composable("LetterToTomorrowScreen"){
